8746519693b7c2bcef946c0eca6d0c85f7995d6d
Boomerrr <38691018+Boomerrr@users.noreply.github.com>
2024-06-26 09:58:31 +0800

    Add files via upload

--- Review_Classify.py
+++ Review_Classify.py
@@ -7,28 +7,28 @@ import wandb
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
-from transformers import BertTokenizer, BertModel, AdamW, RobertaModel, RobertaTokenizer
+from transformers import BertTokenizer, BertModel, AdamW,RobertaModel,RobertaTokenizer
 import torch.nn as nn
 from model import Feature_Aug_Classify, Review_Data_Dataset
 
 
 def parse_args():
     parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument("--model_name", type=str, default="ROBERTA_CLS", help="The path of used model.(Choice : ROBERTA_CLS,BERT_CLS)")
-    parser.add_argument("--dataset", type=str, default="llama3", help="The dataset to use.")
+    parser.add_argument("--model_name", type=str, default="ROBERTA_CLS", help="The path of used model.")
+    parser.add_argument("--dataset", type=str, default="llama3", help="The dataset to use, ATIS or SNIPS.")
     parser.add_argument("--seed", type=int, default=3407, help="The random seed.")
     parser.add_argument("--num_labels", type=int, default=3, help="The number of dataset label.")
     parser.add_argument("--epochs", type=int, default=50, help="The number of iterations for training.")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size for train and validation.")
     parser.add_argument("--max_seq_len", type=int, default=128, help="The max length of sequence.")
-    parser.add_argument("--learning_rate", type=float, default=5e-5, help="Learning rate.")
+    parser.add_argument("--learning_rate", type=float, default=1e-5, help="Learning rate.")
     parser.add_argument("--patience", type=int, default=5, help="Patience for early stop.")
     parser.add_argument("--architecture", type=str, default="Review_Classify", help="Architecture")
-    parser.add_argument("--hidden_size", type=int, default=768, help="The size of gcn output hidden size.")
+    parser.add_argument("--hidden_size", type=int, default=768, help="The size of hidden size.")
+    parser.add_argument("--weight_decay", type=int, default=1e-4, help="The size of hidden size.")
     parser.add_argument("--use_wandb", type=str, default=False, help="Weather use wandb")
     args = parser.parse_args()
     return args
-
 
 args = parse_args()
 
@@ -40,18 +40,19 @@ if use_wandb:
         project="review_classify",
         # track hyperparameters and run metadata
         config={
-            "learning_rate": args.learning_rate,
-            "architecture": args.architecture,
-            "dataset": args.dataset,
-            "epochs": args.epochs,
-            "seed": args.seed,
-            "num_labels": args.num_labels,
-            "batch_size": args.batch_size,
-            "max_seq_len": args.max_seq_len,
-            "patience": args.patience,
-            "model_name": args.model_name,
-            "hidden_szie": args.hidden_size,
-            "add_hidden_size": args.add_hidden_size,
+        "learning_rate": args.learning_rate,
+        "architecture": args.architecture,
+        "dataset": args.dataset,
+        "epochs": args.epochs,
+        "seed":args.seed,
+        "num_labels":args.num_labels,
+        "batch_size":args.batch_size,
+        "max_seq_len":args.max_seq_len,
+        "patience":args.patience,
+        "model_name":args.model_name,
+        "hidden_szie": args.hidden_size,
+        "add_hidden_size": args.add_hidden_size,
+        "weight_decay" : args.weight_decay,
         }
     )
     print(wandb.config)
@@ -67,6 +68,7 @@ epochs = args.epochs
 model = args.model_name
 patience = args.patience
 hidden_size = args.hidden_size
+weight_decay = args.weight_decay
 print("-------------------------------------------------")
 print("The args for train")
 print("dataset:", dataset)
@@ -78,8 +80,10 @@ print("batch_size:", batch_size)
 print("max_length:", max_length)
 print("epochs:", epochs)
 print("patience:", patience)
-print("gcn_hidden_size", hidden_size)
+print("hidden_size",hidden_size)
+print("weight_decay",weight_decay)
 print("-------------------------------------------------")
+
 
 # 固定随机数
 seed = args.seed
@@ -89,53 +93,47 @@ torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.deterministic = True
 
-with open('./data/train_classify_data.pkl', 'rb') as f:
+
+with open('../data/train_classify_data.pkl', 'rb') as f:
     train_tokenized_data = pickle.load(f)
 
-with open('./data/valid_classify_data.pkl', 'rb') as f:
+with open('../data/valid_classify_data.pkl', 'rb') as f:
     valid_tokenized_data = pickle.load(f)
 
-with open('./data/test_classify_data.pkl', 'rb') as f:
+with open('../data/test_filter_classify_data.pkl', 'rb') as f:
     test_tokenized_data = pickle.load(f)
 
-# Test
-# train_tokenized_data = train_tokenized_data[:50]
-# valid_tokenized_data = train_tokenized_data[:50]
-# test_tokenized_data = train_tokenized_data[:50]
-
+#train_tokenized_data = train_tokenized_data[:50]
+#valid_tokenized_data = valid_tokenized_data[:50]
+#test_tokenized_data = test_tokenized_data[:50]
 
 # Tokenizer from transformers
-if model == 'ROBERTA_CLS':
-    model_path = '../roberta_base'
+if model=='ROBERTA_CLS':
+    model_path = '../model/roberta_base'
     tokenizer = RobertaTokenizer.from_pretrained(model_path)
-    model = Feature_Aug_Classify(model, model_path=model_path, hidden_size=hidden_size, head_num=12, pos=True,
-                                 content=True, dependency=True)
-elif model == 'BERT_CLS':
-    model_path = './bert_base'
+    model = Feature_Aug_Classify(model,model_path=model_path,hidden_size = hidden_size,head_num = 12,pos = True,content = True,dependency = True)
+elif model=='BERT_CLS':
+    model_path = '../model/bert_base'
     tokenizer = BertTokenizer.from_pretrained(model_path)
-    model = Feature_Aug_Classify(model, model_path=model_path, hidden_size=hidden_size, head_num=12, pos=True,
-                                 content=True, dependency=True)
+    model = Feature_Aug_Classify(model,model_path=model_path,hidden_size = hidden_size,head_num = 12,pos = True ,content = True,dependency = True)
 
 # Create dataset and data loader
-train_dataset = Review_Data_Dataset(train_tokenized_data, tokenizer)
-train_data_loader = DataLoader(train_dataset, batch_size=batch_size)
+train_dataset = Review_Data_Dataset(train_tokenized_data,tokenizer)
+train_data_loader = DataLoader(train_dataset, batch_size=args.batch_size)
 
-valid_dataset = Review_Data_Dataset(valid_tokenized_data, tokenizer)
-valid_data_loader = DataLoader(valid_dataset, batch_size=batch_size)
+valid_dataset = Review_Data_Dataset(valid_tokenized_data,tokenizer)
+valid_data_loader = DataLoader(valid_dataset, batch_size=args.batch_size)
 
-test_dataset = Review_Data_Dataset(test_tokenized_data, tokenizer)
-test_data_loader = DataLoader(test_dataset, batch_size=batch_size)
+test_dataset = Review_Data_Dataset(test_tokenized_data,tokenizer)
+test_data_loader = DataLoader(test_dataset, batch_size=args.batch_size)
 
-print("Training set length:", len(train_dataset))
-print("Validation set length:", len(valid_dataset))
-print("Test set length:", len(test_dataset))
 print("Data prepared ! ")
 
 model.to('cuda')
 # Use PyTorch's AdamW instead of the one from transformers
-optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-best_model = None
+best_model=None
 patience = args.patience
 best_valid_f1 = 0.0
 early_stopping_counter = 0
@@ -164,9 +162,8 @@ for epoch in range(epochs):
             'attention_mask': attention_mask_dep
         }
         labels = batch['labels'].to('cuda')
-        outputs = model(content_input, pos_input, dependency_input)
-        loss = nn.CrossEntropyLoss()(outputs, labels)
-
+        outputs = model(content_input, pos_input,dependency_input)
+        loss = nn.CrossEntropyLoss()(outputs,labels)
         loss.backward()
         optimizer.step()
         train_loss += loss.item() * input_ids.size(0)
@@ -197,8 +194,7 @@ for epoch in range(epochs):
                 'input_ids': input_ids_dep,
                 'attention_mask': attention_mask_dep
             }
-            outputs = model(content_input, pos_input, dependency_input)
-
+            outputs = model(content_input,pos_input,dependency_input)
             _, predicted = torch.max(outputs, dim=1)
             y_true.extend(labels.cpu().numpy())
             y_pred.extend(predicted.cpu().numpy())
@@ -214,12 +210,12 @@ for epoch in range(epochs):
     else:
         early_stopping_counter += 1
         if early_stopping_counter >= patience:
-            print(f"Early stopping triggered! Stop training.,Current epoch is {epoch + 1}")
+            print(f"Early stopping triggered! Stop training.,Current epoch is {epoch+1}")
             break
 model.load_state_dict(best_model)
 model.eval()
 
-torch.save(model.state_dict(), './saved_model/1_1_1_review_classify_model_roberta.pth')
+torch.save(model.state_dict(), '../saved_model/1_1_1_review_classify_model_roberta.pth')
 
 all_labels = []
 all_predictions = []
@@ -231,7 +227,7 @@ with torch.no_grad():
         attention_mask_pos = batch['attention_mask_pos'].to('cuda')
         input_ids_dep = batch['input_ids_dep'].to('cuda')
         attention_mask_dep = batch['attention_mask_dep'].to('cuda')
-        labels = batch['labels'].to('cuda')
+        labels = batch['labels'].to('cuda') 
         content_input = {
             'input_ids': input_ids,
             'attention_mask': attention_mask
@@ -244,10 +240,11 @@ with torch.no_grad():
             'input_ids': input_ids_dep,
             'attention_mask': attention_mask_dep
         }
-        outputs = model(content_input, pos_input, dependency_input)
-        _, predicted = torch.max(outputs, dim=1)
+        outputs = model(content_input,pos_input,dependency_input)
+        _,predicted = torch.max(outputs, dim=1)
         all_labels.extend(labels.cpu().numpy())
         all_predictions.extend(predicted.cpu().numpy())
+
 
 accuracy = accuracy_score(all_labels, all_predictions)
 precision = precision_score(all_labels, all_predictions, average='macro')
@@ -265,4 +262,5 @@ with open(f'./saved_model/1_1_1_roberta_predictions.pkl', 'wb') as f:
     pickle.dump(all_predictions, f)
 
 if use_wandb:
-    wandb.log({"Test Accuracy": accuracy, "Test Precision": precision, "Test Recall": recall, "Test F1 Score": f1})
+    wandb.log({"Test Accuracy": accuracy, "Test Precision": precision,"Test Recall":recall,"Test F1 Score":f1})
+
--- model.py
+++ model.py
@@ -8,18 +8,16 @@ from tqdm import tqdm
 from transformers import BertModel, RobertaModel
 import sys
 import os
-
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 class Review_Data_Dataset(Dataset):
     def __init__(self, tokenized_data, tokenizer):
         # (sent, tokenized_sentence, tree,label)
         self.encodings_word = [
-            tokenizer(sentence[0], truncation=True, padding='max_length', max_length=128, return_tensors="pt") for
-            sentence in tqdm(tokenized_data, desc="Encoding Words")
+            tokenizer(sentence[0], truncation=True, padding='max_length', max_length=128, return_tensors="pt")  for sentence in tqdm(tokenized_data, desc="Encoding Words")
         ]
         self.encodings_pos = [
-            tokenizer(" ".join([token[0] for token in sentence[1]]), truncation=True, padding='max_length',
-                      max_length=128, return_tensors="pt")
+            tokenizer(" ".join([token[0] for token in sentence[1]]), truncation=True, padding='max_length', max_length=128,return_tensors="pt")
             for sentence in tqdm(tokenized_data, desc="Encoding POS")
         ]
         self.encodings_dependency = [
@@ -28,6 +26,7 @@ class Review_Data_Dataset(Dataset):
             for sentence in tqdm(tokenized_data, desc="Encoding Dependencies")
         ]
         self.labels = [sentence[3] for sentence in tokenized_data]
+
 
     def __len__(self):
         return len(self.labels)
@@ -39,10 +38,9 @@ class Review_Data_Dataset(Dataset):
         item['input_ids_dep'] = self.encodings_dependency[idx]['input_ids'].squeeze(0)
         item['attention_mask_dep'] = self.encodings_dependency[idx]['attention_mask'].squeeze(0)
         item['labels'] = torch.tensor(self.labels[idx], dtype=torch.long)
-        # for key, value in item.items():
+        #for key, value in item.items():
         #    print(f"{key}: {value.shape}")
         return item
-
 
 class Reiveiw_Classify(nn.Module):
     def __init__(self, input_size):
@@ -55,7 +53,6 @@ class Reiveiw_Classify(nn.Module):
         x = torch.relu(x)
         x = self.fc1(x)
         return x
-
 
 class GCN(nn.Module):
     def __init__(self, in_features, out_features, bias=True):
@@ -89,75 +86,86 @@ class GCN(nn.Module):
 
 
 class Feature_Aug_Classify(nn.Module):
-    def __init__(self, model, model_path, hidden_size, head_num, pos, content, dependency):
+    def __init__(self,model, model_path,hidden_size,head_num,pos,content,dependency):
         super(Feature_Aug_Classify, self).__init__()
         if model == 'BERT_CLS':
             self.bert_content = BertModel.from_pretrained(model_path)
             self.bert_pos = BertModel.from_pretrained(model_path)
             self.bert_dependency = BertModel.from_pretrained(model_path)
-        else:
+        else :
             self.bert_content = RobertaModel.from_pretrained(model_path)
             self.bert_pos = RobertaModel.from_pretrained(model_path)
             self.bert_dependency = RobertaModel.from_pretrained(model_path)
-        self.head_num = head_num
-        self.gcns_content = nn.ModuleList([GCN(768, hidden_size, True) for _ in range(head_num)])
+        self.head_num = head_num 
+        self.gcns_word = nn.ModuleList([GCN(768, hidden_size, True) for _ in range(head_num)])
         self.gcns_pos = nn.ModuleList([GCN(768, hidden_size, True) for _ in range(head_num)])
         self.gcns_dependency = nn.ModuleList([GCN(768, hidden_size, True) for _ in range(head_num)])
-
-        count = sum([pos, content, dependency])
-        self.classify = Reiveiw_Classify(768 * count)
+        
         self.pos = pos
         self.content = content
         self.dependency = dependency
+        
+        count = sum([pos, content, dependency])
+        self.classify = Reiveiw_Classify(hidden_size * count)
+        self.bn = torch.nn.BatchNorm1d(hidden_size*count)
         self.drop = nn.Dropout(0.1)
 
     def forward(self, content_input, pos_input, dependency_input):
-        feature_content = None
+        logit_content = None
+        logit_pos = None
+        logit_dependency = None
+        feature_content=None
         feature_pos = None
         feature_dependency = None
+        outputs_to_concat = []
         # Word
         if self.content:
             content_output = self.bert_content(**content_input, output_attentions=True)
             content_attention = content_output.attentions
             content_hidden = content_output.last_hidden_state
-            feature_content = self.process_attention(content_attention, content_hidden, self.gcns_content,
-                                                     content_input['attention_mask'])
+            feature_content = self.process_attention(content_attention, content_hidden, self.gcns_word, content_input['attention_mask'])
+            outputs_to_concat.append(feature_content)
         # Pos
         if self.pos:
             pos_output = self.bert_pos(**pos_input, output_attentions=True)
             pos_attention = pos_output.attentions
             pos_hidden = pos_output.last_hidden_state
             feature_pos = self.process_attention(pos_attention, pos_hidden, self.gcns_pos, pos_input['attention_mask'])
-
+            outputs_to_concat.append(feature_pos)
         # Dependency
         if self.dependency:
             dependency_output = self.bert_dependency(**dependency_input, output_attentions=True)
             dependency_attention = dependency_output.attentions
             dependency_hidden = dependency_output.last_hidden_state
-            feature_dependency = self.process_attention(dependency_attention, dependency_hidden, self.gcns_dependency,
-                                                        dependency_input['attention_mask'])
-
-        output = torch.cat((feature_content, feature_pos, feature_dependency), dim=1)
-        # output = torch.cat((feature_pos,feature_dependency), dim=1)
-        # print(output.shape)
+            feature_dependency = self.process_attention(dependency_attention, dependency_hidden, self.gcns_dependency, dependency_input['attention_mask'])
+            outputs_to_concat.append(feature_dependency)
+        if outputs_to_concat:
+            output = torch.cat(outputs_to_concat, dim=1)
+            output = self.bn(output)
         logit = self.classify(output)
         return logit
 
     def process_attention(self, attentions, hidden_state, gcns, atten_mask):
-        k = 3
         attentions = attentions[-1]
         # print(f'attentions.shape : {attentions.shape}')
-        out_heads = []
-        # Multi head attention matrix GCN
+        word_out_heads = []
         for head in range(self.head_num):
             out = gcns[head].forward(attentions[:, head, :, :], hidden_state)
-            out_heads.append(out)
-        output = torch.stack(out_heads, dim=1)
-        topk_indices = torch.topk(output, k=k, dim=1)[1]
-        topk_values = torch.gather(output, 3, topk_indices)
-        out = torch.mean(topk_values, dim=1)
+            word_out_heads.append(out)
+        output = torch.stack(word_out_heads, dim=1)
+        
+        # k-max pool 
+        #k=3
+        #topk_indices = torch.topk(output, k=k, dim=1)[1]
+        #topk_values = torch.gather(output, 3, topk_indices)
+        #out = torch.mean(topk_values, dim=1)
+        
+        # max pool
+        #out,_  = torch.max(output,dim=1)
 
-        # Pool token layer
+        # mean pool
+        out = torch.mean(output, dim=1)
+
         attention_mask_expanded = atten_mask.unsqueeze(-1).expand(out.size()).float()
         sum_embeddings = torch.sum(out * attention_mask_expanded, dim=1)
         sum_mask = torch.clamp(attention_mask_expanded.sum(dim=1), min=1e-9)
